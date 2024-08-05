c9a66cda4c862103bafbbfb95882a403e9e02b6e
pl9shy <neo.wwh1@gmail.com>
2024-08-05 21:55:12 +0500

    Second Commit

--- obj/Debug/net8.0/Server Monitoring.AssemblyInfo.cs
+++ obj/Debug/net8.0/Server Monitoring.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server Monitoring")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5326a0f6cb942e65e0de6c7a3d958d7a2d8a2f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b56efb632d828fdbe2250e774cb6ed9038976c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server Monitoring")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server Monitoring")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
